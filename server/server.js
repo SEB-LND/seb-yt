0fa1750674ded0be5ad6be687dbdb9c24be2e421
Nana0828 <bcs22020008@student.uts.edu.my>
2025-10-28 11:14:52 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -64,5 +64,5 @@ app.post("/api/videos", async (req, res) => {
   }
 });
 
-const PORT = 8080;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
