d14d224673625c9ae6bbb2b994b60e32ad91153d
Nana0828 <bcs22020008@student.uts.edu.my>
2025-11-04 08:48:27 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,22 +10,22 @@ import HistoryPage from './pages/HistoryPage';
 import PlaylistPage from './pages/PlaylistPage';
 import WatchLaterPage from './pages/WatchLaterPage'; 
 import LikedVideosPage from './pages/LikedVideosPage';
-import axios from 'axios';
+import { supabase } from './supabaseClient.ts';
 
 function App() {
   const isMobileView = useIsMobileView();
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    const fetchAPI = async () => {
-      try {
-        const response = await axios.get('http://localhost:8080/api/videos'); 
-        setVideos(response.data);
-      } catch (error) {
+    const fetchVideos = async () => {
+      const { data, error } = await supabase.from('videos').select('*');
+      if (error) {
         console.error("Error fetching videos:", error);
+      } else {
+        setVideos(data);
       }
     };
-    fetchAPI();
+    fetchVideos();
   }, []);
 
   return (
@@ -34,7 +34,6 @@ function App() {
         <Header />
         <Switch>
           <Route exact path="/">
-            {/* pass videos from backend to Main */}
             <Main videos={videos} />
           </Route>
           <Route path="/results" component={SearchPage} />
